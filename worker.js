a0d2463a502a2343736cfc100a8438c9a5844c9c
c85TKN2Gu5JT <106869448+c85TKN2Gu5JT@users.noreply.github.com>
2022-06-04 12:04:34 +0000

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'c3lefherf.herokuapp.com'
+      url.hostname = 'c4ujyyj.herokuapp.com'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
